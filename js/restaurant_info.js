1d807e89429775bff53f07911bac0e068369f10d
ContemplativeCoffeeCup <michellechristinejohnson@gmail.com>
2018-10-01 05:59:26 -0700

    Adding alt text to images for restaurant information pages

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -91,6 +91,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'A photo of ' + restaurant.name + '\'s storefront';
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
