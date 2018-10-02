0b9723b9d28192aaa260503c6ab0a6434d6b67e6
ContemplativeCoffeeCup <michellechristinejohnson@gmail.com>
2018-10-01 23:14:58 -0700

    Finished service worker fetch event

--- js/main.js
+++ js/main.js
@@ -217,25 +217,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 /*
 Set up service worker
 */
-// if ('serviceWorker' in navigator) {
-//   // Register a service worker hosted at the root of the
-//   // site using the default scope.
-//   navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//     console.log('Service worker registration succeeded:', registration);
-//   }, /*catch*/ function(error) {
-//     console.log('Service worker registration failed:', error);
-//   });
-// } else {
-//   console.log('Service workers are not supported.');
-// }
-
-
-
 
 if ('serviceWorker' in navigator) {
   // navigator.serviceWorker
-  navigator.serviceWorker.register('/sw.js')
-  navigator.serviceWorker.catch(function(err) {
+  navigator.serviceWorker
+  .register('/sw.js')
+  .catch(function(err) {
     console.error(err);
   });
 }
--- sw.js
+++ sw.js
@@ -29,3 +29,29 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+
+self.addEventListener('fetch', function(e) {
+  e.waitUntil(
+    caches.match('e.request').then(function(response) {
+      if (response) {
+        console.log('Found ', e.request, ' in cache');
+        return response;
+      }
+      else {
+        console.log('Could not find ', e.request, ' in cache, FETCHING!');
+        return fetch(e.request)
+        .then(function(response) {
+          const clonedResponse = response.clone();
+          caches.open('v1').then(function(cache) {
+            cache.put(e.request, clonedResponse);
+          })
+          return response;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+      }
+    })
+  );
+});
